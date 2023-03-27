39388cbb711a27372f687a5ca5334c3302326735
Yesica Perlaza <perlazayesik@gmail.com>
2023-03-27 18:20:52 -0500

    calculadora completa

--- js/main.js
+++ js/main.js
@@ -1,44 +1,43 @@
 (function(){
   "use strict"
   const d = document;
-  const numeros = [...d.querySelectorAll(".btn-num")];
-  const dato = d.querySelector(".datos");
-  
-  let suma = 0, mult = 1, div = 0, rest = 0;
-  let num=0, resultado;
+  const datosIngresados = [...d.querySelectorAll(".btn-num")];
+  const datosVer = d.querySelector(".datos");
+
+  let num = 0, resultado;
 
 
-  numeros.forEach(e => {
+  datosIngresados.forEach(e => {
 
     e.addEventListener("click", el =>{
-      if(e.textContent == "CE"){
-        dato.value = "";
-        num = 0;
-      }
-      if(e.textContent == "borrar"){
-      }
 
-      if(e.textContent >= 0 && e.textContent <= 9){
-        dato.value += e.textContent;
+      if(e.textContent <=9){
+        datosVer.value += e.textContent;
         num += e.textContent;
 
       }
-      
-      if(e.textContent == "+"){
-        dato.value += e.textContent;
-        suma += Number(num);
-        resultado = suma;
+
+      if(e.textContent == "+" || e.textContent == "-" || e.textContent == "*" || e.textContent == "/" || e.textContent == "%"){
+        datosVer.value += e.textContent;
+        num += e.textContent;
       }
-      
 
       if(e.textContent == "="){
-        dato.value = "";
-        dato.value = resultado;
+        datosVer.value = "";
+        resultado = num.slice(1);
+        datosVer.value += eval(resultado);
       }
-      console.log(num);
-      console.log(resultado);
+
+      if(e.textContent == "CE"){
+        datosVer.value = "";
+        num = 0;
+      }
+
     });
 
   })
+
+
+ 
 
 })();
